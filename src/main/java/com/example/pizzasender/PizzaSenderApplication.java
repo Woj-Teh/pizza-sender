c3e81124517f8b53699126a8b982cf7ea5a232f5
Z6WOD <wod@kmd.dk>
2023-08-01 23:40:28 +0200

    first commit

---
+++ src/main/java/com/example/pizzasender/PizzaSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzasender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaSenderApplication.class, args);
+    }
+
+}
